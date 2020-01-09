801b53d2445a0e2d57717773528a01735268e260
Max Glova <maxwellglova@gmail.com>
2020-01-09 15:23:07 -0800

    design sync update

--- src/components/Splash-Header/splashHeader.js
+++ src/components/Splash-Header/splashHeader.js
@@ -7,7 +7,6 @@ import logo from "../../images/pantheon-logo.svg"
 import regArrow from "../../images/btn-arrow-right.png"
 import videoBtn from "../../images/splashVideoBtn.png"
 import "./splashHeader.scss"
-import "//fast.wistia.com/assets/external/E-v1.js"
 
 
 console.log(logo);
@@ -40,18 +39,6 @@ const Header = ({ siteTitle }) => (
     </Link>
     <h1 className="bold-upper-head">SCALE YOUR BUSINESS. <br />REDESIGN AN INDUSTRY.</h1>
   </div>
-
-  <div class="col-md-12" style="padding: 0px;">
-            <script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js" async=""></script>
-            <div class="wistia_embed wistia_async_soae5795cd popover=true popoverContent=html autoplay=true wistia_embed_initialized" style="display:inline-block; white-space:nowrap;" id="wistia-soae5795cd"><div id="wistia_29.thumb_container" class="wistia_click_to_play" style="position: relative; height: 237.969px; width: 342.986px;">
-              <div id="wistia_13.thumb_container" style="">
-              <div>
-              <img src="https://get.servicetitan.com/rs/304-EAE-038/images/chrisHunterVideoThumbnail.png" className="splashVideoBtn" />
-              </div>
-            </div>
-            </div>
-            </div>
-          </div>
 
 
   </div>
