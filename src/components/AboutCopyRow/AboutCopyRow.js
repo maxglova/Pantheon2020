9323d0c6a71ef63ff833bf85ca80020da364f44d
Max Glova <maxwellglova@gmail.com>
2020-01-13 09:56:34 -0800

    presentation error fixed

--- src/components/AboutCopyRow/AboutCopyRow.js
+++ src/components/AboutCopyRow/AboutCopyRow.js
@@ -3,13 +3,12 @@ import PropTypes from "prop-types"
 import React from "react"
 import "./style.scss"
 import blueArrow from "../../images/blue-arrow.png"
-import Image from 'react-bootstrap/Image'
 import calendarIcon from "../../images/Icons/calendar.png"
 import locationIcon from "../../images/Icons/location-pin.png"
 import priceIcon from "../../images/Icons/money-currancy.png"
 import infoIcon from "../../images/Icons/information.png"
-import rightDashedLines from "../../images/icons/aboutRightDashedLine.png"
-import bottomDashedLines from "../../images/icons/aboutBottomDashedLine.png"
+import rightDashedLines from "../../images/Icons/aboutRightDashedLine.png"
+import bottomDashedLines from "../../images/Icons/aboutBottomDashedLine.png"
 
 
 const AboutCopyRow = (props) => (
@@ -21,7 +20,7 @@ const AboutCopyRow = (props) => (
           <p className="bodyTxt-noBorder">In ancient Greece, it was said that all-powerful Titans would assemble at the PANTHEON to make plans to protect mankind. Today, ServiceTitan considers men and women of the home and commercial service industry to be modern day Titans…</p>
           <h4 className="bold-upper-head medium-content-spacer">What’s New at Pantheon 2020?</h4>
           <p className="bodyTxt-noBorder">Last year, we transformed attendees into better leaders and entrepreneurs. In 2020, we’re providing them the instruction and know-how to futureproof their success. Join us for two days of training and insight to plan out the next stage of your business… and the industry at large.</p>
-          <a className="textLink-wArrow small-content-spacer" href="#">Manage Sessions <img src={blueArrow} /></a>
+          <a className="textLink-wArrow small-content-spacer" href="#">Manage Sessions <img src={blueArrow} alt="Link Arrow"/></a>
         </div>
         <div className="col-sm-2 noShowMobile"></div>
         <div className="col-sm-4">
@@ -58,8 +57,8 @@ const AboutCopyRow = (props) => (
               </div>
             </div>
           </div>
-          <img className="aboutDetailsRightDashedLines" src={rightDashedLines} />
-          <img className="aboutDetailsBottomDashedLines" src={bottomDashedLines} />
+          <img className="aboutDetailsRightDashedLines" src={rightDashedLines} alt="image-border-graphic"/>
+          <img className="aboutDetailsBottomDashedLines" src={bottomDashedLines} alt="image-border-graphic"/>
         </div>
       </div>
     </div>
--- src/components/Carousel/carousel.js
+++ src/components/Carousel/carousel.js
@@ -1,5 +1,3 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import Carousel from 'react-bootstrap/Carousel'
 import torchCarousel from '../../images/Carousel-Imgs/carousel-torch.png'
@@ -23,71 +21,71 @@ const CarouselComponent = ({ siteTitle }) => (
             alt="First slide"
             />
             <Carousel.Caption>
-            <img className="caption-top-bracket" src={topBracket} />
+            <img className="caption-top-bracket" src={topBracket} alt="quote border top bracket"/>
             <p className="firstQuote">"<span className="secondQuote">"</span></p>
             <p className="quote">Honestly, it's the networking opportunities. It's getting to know the other Titans at Pantheon and learning how they use the software. That’s what I get most excited about.”</p>
             <p className="quoteeName">Stephanie Hornbeck, Accounting Supervisor for Earl’s Plumbing</p>
-            <img className="caption-bottom-bracket" src={bottomBracket} />
+            <img className="caption-bottom-bracket" src={bottomBracket} alt="Quote border bottom bracket"/>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-100"
             src={torchCarousel}
-            alt="Third slide"
+            alt="Torch Network"
             />
 
             <Carousel.Caption>
-            <img className="caption-top-bracket" src={topBracket} />
+            <img className="caption-top-bracket" src={topBracket} alt="quote border top bracket"/>
             <p className="firstQuote">"<span className="secondQuote">"</span></p>
             <p className="quote">I can't say enough about ServiceTitan. That's why I'm a Torch member. It's great software—plus the team and their customer service has been fantastic!”</p>
             <p className="quoteeName">Dave Smith, Owner of World Home Services</p>
-            <img className="caption-top-bracket" src={bottomBracket} />
+            <img className="caption-top-bracket" src={bottomBracket} alt="Quote border bottom bracket"/>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-100"
             src={trainingCarousel}
-            alt="Third slide"
+            alt="Pantheon Training"
             />
 
             <Carousel.Caption>
-            <img className="caption-top-bracket" src={topBracket} />
+            <img className="caption-top-bracket" src={topBracket} alt="quote border top bracket"/>
             <p className="firstQuote">"<span className="secondQuote">"</span></p>
             <p className="quote">I feel like in all the sessions I got little nuggets that I could take back and dive in deeper with with my Success Manager or just on my own.” </p>
             <p className="quoteeName">Janelle, Manager at High Air Conditioning Electrical Service</p>
-            <img className="caption-bottom-bracket" src={bottomBracket} />
+            <img className="caption-bottom-bracket" src={bottomBracket} alt="Quote border bottom bracket"/>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-100"
             src={speakersCarousel}
-            alt="Third slide"
+            alt="Keynote Speakers"
             />
 
             <Carousel.Caption>
-            <img className="caption-top-bracket" src={topBracket} />
+            <img className="caption-top-bracket" src={topBracket} alt="quote border top bracket"/>
             <p className="firstQuote">"<span className="secondQuote">"</span></p>
             <p className="quote">There is a lot of good things that we learn from the speakers. They're very polished and I love how it was presented.”</p>
             <p className="quoteeName">Sheron Cresson, Finance Manager at Blackhawk Heating</p>
-            <img className="caption-bottom-bracket" src={bottomBracket} />
+            <img className="caption-bottom-bracket" src={bottomBracket} alt="Quote border bottom bracket"/>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-100"
             src={partyCarousel}
-            alt="Third slide"
+            alt="Pantheon after party"
             />
 
             <Carousel.Caption>
-            <img className="caption-top-bracket" src={topBracket} />
+            <img className="caption-top-bracket" src={topBracket} alt="quote border top bracket"/>
             <p className="firstQuote">"<span className="secondQuote">"</span></p>
             <p className="quote long-quote-mobile">I’ve been able to network with other contractors who have gone through what I'm currently going through or vice versa. Like I'm able to help another contractor that's going through something that I’ve been able to accomplish and have already gotten over that hurdle.”</p>
             <p className="quoteeName">McKay, Office Manager at Friendly Home Services</p>
-            <img className="caption-bottom-bracket" src={bottomBracket} />
+            <img className="caption-bottom-bracket" src={bottomBracket} alt="quote border top bracket"/>
             </Carousel.Caption>
         </Carousel.Item>
     </Carousel>
